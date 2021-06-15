15a0fb5233037f585c292e0a47799ef7b1cc4aaf
Pmso <pedro-mig@hotmail.com>
2021-06-15 23:45:28 +0100

    Some work in the grape block Need a lot of code cleaning

--- src/main/java/com/pmso/tugaflavours/client/blocks/GrapeBlock.java
+++ src/main/java/com/pmso/tugaflavours/client/blocks/GrapeBlock.java
@@ -1,28 +1,45 @@
 package com.pmso.tugaflavours.client.blocks;
 
+import java.util.Map;
+import java.util.Random;
+
 import com.pmso.tugaflavours.client.items.GrapeItem;
+import com.pmso.tugaflavours.util.SurroundingBlocksUtils;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.BushBlock;
+import net.minecraft.block.IGrowable;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.state.EnumProperty;
 import net.minecraft.state.IntegerProperty;
-import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.fml.RegistryObject;
 
-public class GrapeBlock extends Block{
+public class GrapeBlock extends BushBlock implements IGrowable{
 	
-	public static final IntegerProperty AGE = BlockStateProperties.AGE_0_3;
+	public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 2);
+
+	private static final int minDrop=1;
+	private static final int maxDrop=3;
 
 
 	protected static VoxelShape SHAPE = Block.makeCuboidShape(4.0D, 4.0D, 4.0D, 12.0D, 16.0D, 12.0D);
@@ -34,7 +51,12 @@ public class GrapeBlock extends Block{
 				.zeroHardnessAndResistance()
 				.sound(SoundType.CROP)
 				.notSolid()
-				.harvestLevel(1));
+				.harvestLevel(1)
+				.tickRandomly()
+				.doesNotBlockMovement());
+		
+		this.setDefaultState(this.stateContainer.getBaseState()
+                .with(AGE, 0));
 		
 		this.grape=redGrapes;
 	}
@@ -44,10 +66,10 @@ public class GrapeBlock extends Block{
 	public void onPlayerDestroy(IWorld worldIn, BlockPos pos, BlockState state) {
 		// TODO Auto-generated method stub
 		
-		String biome=Minecraft.getInstance().world.getBiome(pos).getRegistryName().toString().replace(":", ".");
+		String biome=worldIn.getBiome(pos).getRegistryName().toString().replace(":", ".");
 		
 		
-		ItemStack stack= new ItemStack(this.grape.get(), 1);
+		ItemStack stack= new ItemStack(this.grape.get(), minDrop);
 		
 		CompoundNBT nbt;
 	    if (stack.hasTag())
@@ -75,7 +97,145 @@ public class GrapeBlock extends Block{
 	}
 	
 	@Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+        if (state.get(AGE) == this.getMaxAge()) {
+            int count = RANDOM.nextInt(maxDrop - minDrop) + minDrop;
+            // Spawn the random drop count
+            ItemStack itemStack = new ItemStack(this.grape.get(), count);
+            InventoryHelper.spawnItemStack(worldIn, pos.getX(), pos.getY(), pos.getZ(), itemStack);
+
+            // Decrease age to 0
+            worldIn.setBlockState(pos, this.getActualBlockStateWithAge(worldIn, pos, 0), 2);
+        }
+        return ActionResultType.PASS;
+    }
+	
+	@Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return SHAPE;
+    }
+	
+	@Override
+    public void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(AGE);
+    }
+	
+	public boolean isMaxAge(BlockState state) {
+        return state.get(this.getAgeProperty()) >= this.getMaxAge();
+    }
+	
+	public IntegerProperty getAgeProperty() {
+        return AGE;
+    }
+
+    public int getMaxAge() {
+        return 2;
+    }
+    
+    protected int getAge(BlockState state) {
+        return state.get(this.getAgeProperty());
+    }
+
+    public BlockState withAge(int age) {
+        return this.getDefaultState().with(this.getAgeProperty(), age);
+    }
+    
+    @Override
+    public boolean canGrow(IBlockReader worldIn, BlockPos pos, BlockState state, boolean isClient) {
+        return !this.isMaxAge(state);
+    }
+
+
+	@Override
+	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, BlockState state) {
+		 return !this.isMaxAge(state);
+	}
+	
+	protected static float getGrowthChance(Block blockIn, IBlockReader worldIn, BlockPos pos) {
+        float f = 1.0F;
+        BlockPos blockpos = pos.down();
+
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                float f1 = 0.0F;
+                BlockState blockstate = worldIn.getBlockState(blockpos.add(i, 0, j));
+                if (blockstate.canSustainPlant(worldIn, blockpos.add(i, 0, j), Direction.UP, (net.minecraftforge.common.IPlantable) blockIn)) {
+                    f1 = 1.0F;
+                    if (blockstate.isFertile(worldIn, blockpos.add(i, 0, j))) {
+                        f1 = 3.0F;
+                    }
+                }
+
+                if (i != 0 || j != 0) {
+                    f1 /= 4.0F;
+                }
+
+                f += f1;
+            }
+        }
+
+        BlockPos blockpos1 = pos.north();
+        BlockPos blockpos2 = pos.south();
+        BlockPos blockpos3 = pos.west();
+        BlockPos blockpos4 = pos.east();
+        boolean flag = blockIn == worldIn.getBlockState(blockpos3).getBlock() || blockIn == worldIn.getBlockState(blockpos4).getBlock();
+        boolean flag1 = blockIn == worldIn.getBlockState(blockpos1).getBlock() || blockIn == worldIn.getBlockState(blockpos2).getBlock();
+        if (flag && flag1) {
+            f /= 2.0F;
+        } else {
+            boolean flag2 = blockIn == worldIn.getBlockState(blockpos3.north()).getBlock() || blockIn == worldIn.getBlockState(blockpos4.north()).getBlock() || blockIn == worldIn.getBlockState(blockpos4.south()).getBlock() || blockIn == worldIn.getBlockState(blockpos3.south()).getBlock();
+            if (flag2) {
+                f /= 2.0F;
+            }
+        }
+
+        return f;
+    }
+	
+	@Override
+    public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
+        super.tick(state, worldIn, pos, rand);
+        if (!worldIn.isAreaLoaded(pos, 1))
+            return; // Forge: prevent loading unloaded chunks when checking neighbor's light
+        if (worldIn.getLightSubtracted(pos, 0) >= 9) {
+            int i = this.getAge(state);
+            if (i < this.getMaxAge()) {
+                float f = getGrowthChance(this, worldIn, pos);
+                if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, rand.nextInt((int) (25.0F / f) + 1) == 0)) {
+                    grow(worldIn, rand, pos, state);
+                    net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state);
+                }
+            }
+        }
+    }
+
+
+	@Override
+	public void grow(ServerWorld worldIn, Random rand, BlockPos pos, BlockState state) {
+		this.grow(worldIn, pos, state);
+	}
+	
+	public void grow(World worldIn, BlockPos pos, BlockState state) {
+        int i = this.getAge(state) + this.getBonemealAgeIncrease(worldIn);
+        int j = this.getMaxAge();
+        if (i > j) {
+            i = j;
+        }
+
+        worldIn.setBlockState(pos, getActualBlockStateWithAge(worldIn, pos, i), 2);
+    }
+	
+	protected int getBonemealAgeIncrease(World worldIn) {
+        return MathHelper.nextInt(worldIn.rand, 1, 2);
+    }
+	
+	public BlockState getActualBlockStateWithAge(World world, BlockPos blockPos, int age) {
+        Map<String, Block> blockMap = SurroundingBlocksUtils.getSurroundingBlocks(world, blockPos);
+
+        return this.getDefaultState().with(AGE, age);
+    }
+
+    public BlockState getActualBlockState(World world, BlockPos blockPos) {
+        return getActualBlockStateWithAge(world, blockPos, world.getBlockState(blockPos).get(this.getAgeProperty()));
     }
 }
